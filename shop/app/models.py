27de91260a8355c8a3b11f38f7d61c850a2e3265
tongri <antohagryb@gmail.com>
2021-01-22 11:57:31 +0200

    own

--- shop/app/models.py
+++ shop/app/models.py
@@ -9,6 +9,9 @@ class Person(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'People'
+
 
 class Article(models.Model):
     name = models.CharField(max_length=20)
--- shop/app/views.py
+++ shop/app/views.py
@@ -6,20 +6,17 @@ import string
 
 from django.http import HttpResponse
 
+from app.models import Article
+
+
 def first(request):
     return render(request, 'static.html', {
         'name': 'test',
     })
 
-class MyClass:
-    string = ''
-    def __init__(self, s):
-        self.string = s
-
-
 
 def index(request):
-    slugger = ''.join(random.choices(string.ascii_lowercase, k = 5))
+    slugger = ''.join(random.choices(string.ascii_lowercase, k=5))
     article_id = random.randint(0, 99999)
     return render(request, 'static.html', {
         'slugger': slugger,
@@ -27,20 +24,26 @@ def index(request):
         'name': 'main',
     })
 
+
 def articles(request):
+    arts = Article.objects.all()
     return render(request, 'static.html', {
         'name': 'users',
+        'arts': arts,
     })
+
 
 def archive(request):
     return render(request, 'static.html', {
         'name': 'archive page',
     })
 
+
 def users(request, user_num=0):
     return render(request, 'static.html', {
         'name': 'users',
     })
+
 
 def article_number(request, article_id, slug_text=''):
     return render(request, "dynamic.html", {
@@ -48,18 +51,19 @@ def article_number(request, article_id, slug_text=''):
         'slug_text': slug_text,
     })
 
+
 def article_number_archive(request, article_id):
     return render(request, 'articles.html', {
         'article_id': article_id,
     })
 
-def articles(request):
-    return render(request, 'articles.html')
+
 
 def phone(request, phone):
     return render(request, 'phone.html', {
         'phone': phone,
     })
+
 
 def uniq(request, unique):
     return render(request, 'slugger.html', {
